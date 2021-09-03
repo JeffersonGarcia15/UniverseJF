1db3e5a8616ab91b1563f0653e77886924f39808
Jefferson <jeffersongarcia1599@gmail.com>
2021-09-03 03:25:41 -0500

    Changes

--- frontend/src/components/UserPhotos/index.js
+++ frontend/src/components/UserPhotos/index.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import { getSingleUserPhoto, getAllPhotos } from '../../store/photos';
 import { addUserLikeToPhoto, getAllLikes, deleteSingleLike } from '../../store/likes'
 import Comments from '../Comments'
 import UpdateDelePhoto from '../UpdateDeletePhoto'
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import './UserPhotos.css'
 
 function UserPhoto() {
@@ -17,33 +18,47 @@ function UserPhoto() {
     const tags = photo?.Tags
     const likeId = Object.values(likes).find(like => like.userId == user.id && like.photoId == photoId)
     const deleteLike = parseInt(likeId?.id, 10)
+    const photoLength = photo?.Likes?.length
+    const likesInPhoto = Object.values(likes)?.filter(like => like.photoId == photoId)
+    const [deleteSwitch, setDeleteSwitch] = useState(false)
+    const isPhotoLiked = likesInPhoto?.some(like => like.userId === user.id)
+
+    // useEffect(() => {
+    //     dispatch(getSingleUserPhoto(photoId))
+    // }, [dispatch, photoId, photoLength])
+
+    // useEffect(() => {
+    //     dispatch(getAllPhotos())
+    //     dispatch(getAllLikes)
+    // }, [dispatch, likeId, deleteLike])
 
     useEffect(() => {
-        dispatch(getSingleUserPhoto(photoId))
-    }, [dispatch, photoId])
+        dispatch(getAllLikes())
+    }, [dispatch, deleteLike])
 
     useEffect(() => {
         dispatch(getAllPhotos())
-    }, [dispatch])
-
-    useEffect(() => {
-        dispatch(getAllLikes(photoId))
-    }, [dispatch, photoId])
+        dispatch(getAllLikes)
+    }, [])
 
     const addLike = async (e) => {
-        e.preventDefault()
-        const addSingleLikeToPhoto = {
-            photoId: photoId,
-            userId: user.id
-        }
-        console.log('ZzzzzzzzzzZXzzzzzzzzzzzzzzzzzzzzzzzzzzzz', addSingleLikeToPhoto);
-        await dispatch(addUserLikeToPhoto(addSingleLikeToPhoto))
+    e.preventDefault()
+    const addSingleLikeToPhoto = {
+        photoId: photoId,
+        userId: user.id
     }
+    await dispatch(addUserLikeToPhoto(addSingleLikeToPhoto))
+}
 
-    const dislike = async (e) => {
-        e.preventDefault()
-        await dispatch(deleteSingleLike(deleteLike))
-    }
+const dislike = async (e) => {
+    e.preventDefault()
+    await dispatch(deleteSingleLike(deleteLike))
+}
+
+    useEffect(() => {
+        dispatch(getAllPhotos())
+        dispatch(getAllLikes)
+    }, [dispatch, isPhotoLiked])
 
     return (
         <div className='photo--component'>
@@ -53,15 +68,12 @@ function UserPhoto() {
             <div className='color-fondo'>
                 <div className='foto-informacion'>
                     {user.id === photo?.User.id}
-                    <>
-                        <div>
-                            <button className='buton-plane' type='submit' onClick={addLike}>
-                                <i className="fas fa-thumbs-up"></i></button>
-                            <button type='submit' onClick={dislike}><i className="far fa-thumbs-down"></i></button>
-                        </div>
+                    <div>
+
+                        <FavoriteIcon onClick={isPhotoLiked ? dislike : addLike} style={{ color: isPhotoLiked ? 'red' : 'gray', cursor: 'pointer' }} className="icon"></FavoriteIcon>
                         <div className='photo-owner'>
                             <a href={`/profile/${photo?.User.id}`} onClick={e => { e.preventDefault(); history.push(`/profile/${photo?.User.id}`) }}>{photo?.User.firstName}</a>
-                            <p>{photo?.Likes?.length} Like(s)</p>
+                            <p>{photoLength} Like(s)</p>
                             <h3 className='h3-size'>{photo?.title}</h3>
                             <p>{photo?.description}</p>
                             <div>
@@ -72,7 +84,7 @@ function UserPhoto() {
                                 })}
                             </div>
                         </div>
-                    </>
+                    </div>
                 </div>
                 <div>
                     <UpdateDelePhoto></UpdateDelePhoto>
