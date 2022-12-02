b1251080eb07fe322b72a8092fb8fe5680cc24df
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2022-12-02 16:17:47 -0600

    Updating backend port

--- frontend/src/components/ProfileNavBar/EditProfile.js
+++ frontend/src/components/ProfileNavBar/EditProfile.js
@@ -47,41 +47,41 @@ function EditProfile() {
 
     return (
         <div className="form-UpdateProfile">
-            <form onSubmit={onSubmit}>
+            <form onSubmit={onSubmit} className="form-EditProfile">
                 <ul className="form-errors">
                     {errors?.map((error, ind) => <li key={ind}>{error}</li>)}
                 </ul>
                 <h2>Update Profile</h2>
-                <div className="floating-label">
-                    <input
-                        type='text'
-                        name='firstName'
-                        onChange={(e) => setFirstName(e.target.value)}
-                        value={firstName}
-                        required
-                        placeholder='First Name'
-                    ></input>
-                </div>
-                <div className="floating-label">
-                    <input
-                        type='text'
-                        name='lastName'
-                        onChange={updateLastName}
-                        value={lastName}
-                        required
-                        placeholder='Last Name'
-                    ></input>
-                </div>
-                <div className="floating-label">
-                    <input
-                        type='text'
-                        name='username'
-                        onChange={updateUserName}
-                        value={username}
-                        required
-                        placeholder='username'
-                    ></input>
-                </div>
+                <label htmlFor="firstName">First name</label>
+                <input
+                    type='text'
+                    name='firstName'
+                    id='firstName'
+                    onChange={(e) => setFirstName(e.target.value)}
+                    value={firstName}
+                    required
+                    placeholder='First Name'
+                ></input>
+                <label htmlFor="lastName">Last name</label>
+                <input
+                    type='text'
+                    name='lastName'
+                    id='lastName'
+                    onChange={updateLastName}
+                    value={lastName}
+                    required
+                    placeholder='Last Name'
+                ></input>
+                <label htmlFor="userName">User name</label>
+                <input
+                    type='text'
+                    name='username'
+                    id='username'
+                    onChange={updateUserName}
+                    value={username}
+                    required
+                    placeholder='username'
+                ></input>
                 {/* <div className="upload-file">
                     <label>Change Your Profile Picture</label>
                     <input type="file"
