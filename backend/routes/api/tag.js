97bab7fbb9af1f9e115c1f006d446b6ef0dc590b
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2024-09-25 20:05:06 -0500

    Tags/jalg Fixing tags and tags css

--- backend/routes/api/tag.js
+++ backend/routes/api/tag.js
@@ -1,43 +1,59 @@
-const express = require('express')
-const asyncHandler = require('express-async-handler')
-const { requireAuth } = require('../../utils/auth')
-const { User, Photo, Comment, Tag, Tag_Photo } = require('../../db/models');
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const { requireAuth } = require("../../utils/auth");
+const { User, Photo, Comment, Tag, Tag_Photo } = require("../../db/models");
 const router = express.Router();
 
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const tags = await Tag.findAll({});
+    return res.json(tags);
+  })
+);
 
-router.get('/', asyncHandler(async (req, res) => {
-    const tags = await Tag.findAll({})
-    return res.json(tags)
-}))
-
-router.get('/photos/:id(\\d+)', asyncHandler(async (req, res) => {
-    const photoId = parseInt(req.params.id, 10)
+router.get(
+  "/photos/:id(\\d+)",
+  asyncHandler(async (req, res) => {
+    const photoId = parseInt(req.params.id, 10);
     const tags = await Tag.findAll({
-        where: {
-            photoId,
-        }
-    })
-    return res.json(tags)
-}))
+      where: {
+        photoId,
+      },
+    });
+    return res.json(tags);
+  })
+);
 
-
-router.post('/new', requireAuth, asyncHandler(async (req, res) => {
-    const { name } = req.body
+router.post(
+  "/new",
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const { name } = req.body;
+    /**
+     - fetch all existing tags
+     - Check if that array has name on it and if it does return res.json(existingTag);
+     - else create
+     */
     const newTag = await Tag.create({
-        name,
-    })
-    return res.json(newTag)
-}))
+      name,
+    });
+    return res.json(newTag);
+  })
+);
 
-router.post('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
+router.post(
+  "/:id(\\d+)",
+  requireAuth,
+  asyncHandler(async (req, res) => {
     const { photoId, tagId } = req.body;
     const tagInfo = await Tag_Photo.create({
-        photoId,
-        tagId
-    })
+      photoId,
+      tagId,
+    });
 
-    return res.json(tagInfo)
-}))
+    return res.json(tagInfo);
+  })
+);
 
-
-module.exports = router
+module.exports = router;
--- frontend/src/store/tags.js
+++ frontend/src/store/tags.js
@@ -1,113 +1,106 @@
-import { csrfFetch } from './csrf'
+import { csrfFetch } from "./csrf";
 
-const LOAD_ALL_TAGS = 'tags/LOAD_ALL_TAGS'
-const ADD_TO_PHOTO = 'tags/ADD_TO_PHOTO'
-const LOAD_EVERY_TAG = 'tags/LOAD_EVERY_TAG'
-const ADD_SINGLE_TAG = 'tags/ADD_SINGLE_TAG'
+const LOAD_ALL_TAGS = "tags/LOAD_ALL_TAGS";
+const ADD_TO_PHOTO = "tags/ADD_TO_PHOTO";
+const LOAD_EVERY_TAG = "tags/LOAD_EVERY_TAG";
+const ADD_SINGLE_TAG = "tags/ADD_SINGLE_TAG";
 
+const loadTags = (tags) => {
+  return {
+    type: LOAD_ALL_TAGS,
+    tags,
+  };
+};
 
-const loadTags = tags => {
-    return {
-        type: LOAD_ALL_TAGS,
-        tags
-    }
-}
+const addTagToPhoto = (photo) => {
+  return {
+    type: ADD_TO_PHOTO,
+    photo,
+  };
+};
 
-
-const addTagToPhoto = photo => {
-    return {
-        type: ADD_TO_PHOTO,
-        photo
-    }
-}
-
-
-const loadAllTags = tags => {
-    return {
-        type: LOAD_EVERY_TAG,
-        tags
-    }
-}
+const loadAllTags = (tags) => {
+  return {
+    type: LOAD_EVERY_TAG,
+    tags,
+  };
+};
 
 const addSingleTag = (tag) => {
-    return {
-        type: ADD_SINGLE_TAG,
-        tag
-    }
-}
+  return {
+    type: ADD_SINGLE_TAG,
+    tag,
+  };
+};
 
+export const getAllTags = (photoId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/tags/photos/${photoId}`);
+  if (response.ok) {
+    const tag = await response.json();
+    dispatch(loadTags(tag));
+  }
+};
 
-export const getAllTags = photoId => async dispatch => {
-    const response = await csrfFetch(`/api/tags/photos/${photoId}`)
-    if (response.ok) {
-        const tag = await response.json()
-        dispatch(loadTags(tag))
-    }
-}
+export const createTag = (name) => async (dispatch) => {
+  const response = await csrfFetch(`/api/tags/new`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(name),
+  });
+  if (response.ok) {
+    const tag = await response.json();
+    // dispatch(addSingleTag(tag));
+    return tag;
+  }
+};
 
+export const addUserTagToPhoto = (photoInfo) => async (dispatch) => {
+  const response = await csrfFetch(`/api/tags/${photoInfo.photoId}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(photoInfo),
+  });
+  if (response.ok) {
+    const photo = await response.json();
+    dispatch(addTagToPhoto(photo));
+    return photo;
+  }
+};
 
-export const createTag = tagInfo => async dispatch => {
-    const response = await csrfFetch(`/api/tags/new`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(tagInfo)
-    })
-    if (response.ok) {
-        const tag = await response.json()
-        dispatch(addSingleTag(tag))
-    }
-}
+export const getEveryTag = () => async (dispatch) => {
+  const response = await csrfFetch("/api/tags");
+  if (response.ok) {
+    const tags = await response.json();
+    dispatch(loadAllTags(tags));
+  }
+};
 
-
-export const addUserTagToPhoto = photoInfo => async dispatch => {
-    const response = await csrfFetch(`/api/tags/${photoInfo.photoId}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(photoInfo)
-    })
-    if (response.ok) {
-        const photo = await response.json()
-        dispatch(addTagToPhoto(photo))
-    }
-
-}
-
-export const getEveryTag = () => async dispatch => {
-    const response = await csrfFetch('/api/tags')
-    if (response.ok) {
-        const tags = await response.json()
-        dispatch(loadAllTags(tags))
-    }
-}
-
-
-const initialState = {}
+const initialState = {};
 
 export default function tagsReducer(state = initialState, action) {
-    let updatedState = {...state}
-    switch (action.type) {
-        case LOAD_EVERY_TAG: {
-            const newState = {}
-            action.tags.forEach(tag => {
-                newState[tag.id] = tag
-            })
-            return newState
-        }
-        case LOAD_ALL_TAGS: {
-            updatedState[action.tag.id] = action.tag
-            return updatedState
-            // return newState
-        }
-        case ADD_TO_PHOTO: {
-            updatedState[action.photo.photoId] = action.photo
-            return updatedState
-        }
-        default:
-            return state
+  let updatedState = { ...state };
+  switch (action.type) {
+    case LOAD_EVERY_TAG: {
+      const newState = {};
+      action.tags.forEach((tag) => {
+        newState[tag.id] = tag;
+      });
+      return newState;
     }
+    case LOAD_ALL_TAGS: {
+      updatedState[action.tag.id] = action.tag;
+      return updatedState;
+      // return newState
+    }
+    case ADD_TO_PHOTO: {
+      updatedState[action.photo.photoId] = action.photo;
+      return updatedState;
+    }
+    default:
+      return state;
+  }
 }
-
